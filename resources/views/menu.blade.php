9674301601c36d47cab10e308811eaf001063cde
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-08 14:55:09 +0300

    menu blade

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse mb-4">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="/">Flight</a>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Airlines</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Airports</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Countries</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Flights</a>
+            </li>
+        </ul>
+    </div>
+</nav>
