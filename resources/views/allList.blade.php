97b760dadca736b866c23c115120837cce21ce0c
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-08 15:33:41 +0300

    added list blade

--- resources/views/allList.blade.php
+++ resources/views/allList.blade.php
@@ -1,5 +1,74 @@
 @extends('core')
 @section('content')
+    <div class="container">
+        <h2>{{ucfirst($tableName)}}s table</h2>
+        <a href="{{ route($create) }}">Create new {{$tableName}}</a>
+        @if(sizeof($list)>0)
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    @foreach($list[0] as $key => $value)
+                        @if(!in_array($key, $ignore))
+                            <th>{{$key}}</th>
+                        @endif
+                    @endforeach
+                    <th>edit</th>
+                    <th>delete</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($list as $key => $record)
+                    <tr id="{{$record['id']}}">
+                        @foreach($record as $key => $value)
+                            @if(!in_array($key, $ignore))
+                                <td>
+                                    {{$value}}
+                                </td>
+                            @endif
+                        @endforeach
+                        <td>
+                            <a href="{{ route($edit,$record['id']) }}">
+                                <button type="button" class="btn btn-primary">Edit</button>
+                            </a>
+                        </td>
+                        <td>
+                            <button onclick="deleteItem( '{{ route($delete, $record['id']) }}' )"
+                                    class="btn btn-danger">Delete
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @else
+            <h2>Data not find</h2>
+        @endif
+    </div>
 
 
+@endsection
+
+@section('scripts')
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteItem(route) {
+            $.ajax({
+                url: route,
+                type: 'DELETE',
+                dataType: 'json',
+                success: function (response) {
+                    $('#' + response.id).remove();
+                },
+                error: function () {
+                    alert('ERROR')
+                }
+            });
+        }
+
+    </script>
 @endsection
