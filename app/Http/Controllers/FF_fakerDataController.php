005171fa58bafbdc85c02b71925998a5296ee10c
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-10 14:39:44 +0300

    generate id from abc.. array

--- app/Http/Controllers/FF_fakerDataController.php
+++ app/Http/Controllers/FF_fakerDataController.php
@@ -9,12 +9,24 @@ use Illuminate\Http\Request;
 
 class FF_fakerDataController extends Controller
 {
-    public function generateAirports($count = 500)
+    public function generateAirports($count = 100)
     {
+        function getRandomString($length = 3) {
+            $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $airport_id = '';
+
+
+            for ($i = 0; $i < $length; $i++) {
+                $airport_id .= $characters[mt_rand(0, strlen($characters) - 1)];
+            }
+
+            return $airport_id;
+        }
+
         $faker = Factory::create();
         for ($i = 0; $i < $count; $i++) {
            FF_airports::create([
-                'id' => $faker->uuid,
+               'id' => getRandomString(),
                 'name' => $faker->company,
                'city' => $faker->city,
                'contry_id' => FF_contries::all()->random()->id,
@@ -23,3 +35,4 @@ class FF_fakerDataController extends Controller
 
     }
 }
+
